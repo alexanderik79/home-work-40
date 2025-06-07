8088ea4ddb8d4e55dbf8c76aefea5b60fbf4bc3d
alexanderik79 <alexanderik79@gmail.com>
2025-06-07 16:27:52 +0200

    fix Request

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -4,15 +4,24 @@ function Request() {
   const [data, setData] = useState(null);
 
   const apiKey = '0190ed4a-9d93-4b55-ac50-96123dbc39e6';
+  const productId = 1;
+  const company = 'AutoPlus';
 
   useEffect(() => {
-    fetch('https://car-dealer-app.botdepo.shop/api/products/category/1/AutoPlus', {
-        headers: {
-            'X-API-Key': apiKey
-        }
+    fetch(`https://car-dealer-app.botdepo.shop/api/products/product/${productId}/${company}`, {
+      headers: {
+        'X-API-Key': apiKey
+      }
     })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Ошибка при загрузке данных');
+        }
+        return response.json();
+      })
       .then((json) => {
         setData(json);
+
       })
       .catch((error) => {
         console.error('Ошибка:', error);
@@ -21,13 +30,18 @@ function Request() {
 
   return (
     <>
-      {loading ? (
-        <p>Загрузка...</p>
-      ) : (
+      {data ? (
         <div>
           <h3>{data.name}</h3>
-          <p>{data.body}</p>
+          <p>{data.description}</p>
+          <p>Цена: ${data.price}</p>
+          <p>Марка: {data.make}</p>
+          <p>Модель: {data.model}</p>
+          <p>Год: {data.year}</p>
+          <img src={data.photos?.[0]} alt={data.name} width={300} />
         </div>
+      ) : (
+        <p>Нет данных</p>
       )}
     </>
   );
